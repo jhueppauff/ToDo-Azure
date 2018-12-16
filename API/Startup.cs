b261016cf8843ed7536c8beb4a590c1699756ba4
Julian Hüpppauff <julian@hueppauff.info>
2018-12-16 22:28:03 +0100

    added configuration

--- API/Startup.cs
+++ API/Startup.cs
@@ -4,18 +4,33 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+            LoggerFactory = loggerFactory;
+
+            LoggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            Logger = LoggerFactory.CreateLogger<Startup>();
+
+            Logger.Log(LogLevel.Trace, "Startup App");
         }
 
         public IConfiguration Configuration { get; }
+        public ILoggerFactory LoggerFactory { get; }
+        public ILogger Logger { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -34,6 +49,8 @@ namespace API
                     }
                 });
             });
+
+            services.AddSingleton<IConfiguration>(Configuration);
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
